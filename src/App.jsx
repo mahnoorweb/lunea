48c084b8b9a02c8099e1fd01dcee516d1c6beda6
mahnoorweb <hanifmahnoor702@gmail.com>
2026-09-11 23:06:30 -0700

    find errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,39 @@
-import"./App.css"
+// import"./App.css"
+// import { Route, Routes } from "react-router-dom";
+
+// import Navbar from "./Component/Navbar";
+
+// import Home from "./Pages/Home";
+// import Products from "./Pages/Products";
+// import Catagories from "./Pages/Catagories";
+// import ProductDetail from "./Pages/productDetail";
+// import Cart from "./Pages/Cart";
+
+// import Footer from "./Component/Footer";
+
+// const App = () => {
+//   return (
+//     <>
+//       <Navbar />
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/products" element={<Products />} />
+//         <Route path="/categories" element={<Catagories />} />
+
+//         <Route path="/product/:id" element={<ProductDetail />} />
+
+//         <Route path="/cart" element={<Cart />} />
+//       </Routes>
+
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default App;
+
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./Component/Navbar";
--- src/data/Product.js
+++ src/data/Product.js
@@ -1,22 +1,313 @@
+// // const products = [
+// //   // NEW IN
+
+// //   {
+// //     id: 1,
+// //     name: "Blossom Midi",
+// //     price: 4299,
+// //     category: "New In",
+// //         image: "/images/midi.jpg",
+
+// //   },
+// //   {
+// //     id: 2,
+// //     name: "Satin Whisper",
+// //     price: 5899,
+// //     category: "New In",
+// //       image: "/images/satin.jpg",
+// //     // image:"public/images/casual1.jpg"
+// //   },
+// //   {
+// //     id: 3,
+// //     name: "Rose Charm",
+// //     price: 4799,
+// //     category: "New In",
+// //     image: "/images/rose.jpg",
+// //   },
+// //   {
+// //     id: 4,
+// //     name: "Ivory Bloom",
+// //     price: 6299,
+// //     category: "New In",
+// //     image: "/images/rose blosm img.jpg",
+// //   },
+// //   {
+// //     id: 5,
+// //     name: "Velvet Grace",
+// //     price: 5999,
+// //     category: "New In",
+// //     image: "/images/valvet.jpg",
+// //   },
+// //   {
+// //     id: 6,
+// //     name: "Lavender Muse",
+// //     price: 5499,
+// //     category: "New In",
+// //     image: "/images/levender.jpg",
+// //   },
+// //   {
+// //     id: 7,
+// //     name: "Ruby Night",
+// //     price: 6499,
+// //     category: "New In",
+// //     image: "/images/red dress.jpg",
+// //   },
+// //   {
+// //     id: 8,
+// //     name: "Pearl Dress",
+// //     price: 5799,
+// //     category: "New In",
+// //     image: "/images/peral.jpg",
+// //   },
+
+// //   // CASUAL
+
+// //   {
+// //     id: 9,
+// //     name: "Daisy Day Dress",
+// //     price: 3999,
+// //     category: "Casual",
+// //     image: "/images/casual1.jpg",
+// //   },
+// //   {
+// //     id: 10,
+// //     name: "Soft Bloom Midi",
+// //     price: 4299,
+// //     category: "Casual",
+// //     image: "/images/causal5.jpg",
+// //   },
+// //   {
+// //     id: 11,
+// //     name: "Everyday Rose",
+// //     price: 3799,
+// //     category: "Casual",
+// //     image: "/images/casual8.jpg",
+// //   },
+// //   {
+// //     id: 12,
+// //     name: "Cotton Breeze",
+// //     price: 3499,
+// //     category: "Casual",
+// //     image: "/images/casual4.jpg",
+// //   },
+// //   {
+// //     id: 13,
+// //     name: "Pink Meadow",
+// //     price: 4199,
+// //     category: "Casual",
+// //     image: "/images/casual 2.jpg",
+// //   },
+// //   {
+// //     id: 14,
+// //     name: "Blue Serenity",
+// //     price: 4499,
+// //     category: "Casual",
+// //     image: "/images/casual6.jpg",
+// //   },
+// //   {
+// //     id: 15,
+// //     name: "Blush Garden",
+// //     price: 4099,
+// //     category: "Casual",
+// //     image: "/images/casual7.jpg",
+// //   },
+// //   {
+// //     id: 16,
+// //     name: "Lavender Breeze",
+// //     price: 4399,
+// //     category: "Casual",
+// //     image: "/images/casual3.jpg",
+// //   },
+
+// //   // PARTY WEAR
+
+// //   {
+// //     id: 17,
+// //     name: "Midnight Muse",
+// //     price: 5999,
+// //     category: "Party Wear",
+// //     image: "/images/party 1.jpg",
+// //   },
+// //   {
+// //     id: 18,
+// //     name: "Blush Aura",
+// //     price: 5799,
+// //     category: "Party Wear",
+// //     image: "/images/party 2.jpg",
+// //   },
+// //   {
+// //     id: 19,
+// //     name: "Ruby Glow",
+// //     price: 6499,
+// //     category: "Party Wear",
+// //     image: "/image/party3.jpg",
+// //   },
+// //   {
+// //     id: 20,
+// //     name: "Champagne Dream",
+// //     price: 6999,
+// //     category: "Party Wear",
+// //     image: "/image/party4.jpg",
+// //   },
+// //   {
+// //     id: 21,
+// //     name: "Velvet Night",
+// //     price: 6299,
+// //     category: "Party Wear",
+// //     image: "/image/party5.jpg",
+// //   },
+// //   {
+// //     id: 22,
+// //     name: "Rose Sparkle",
+// //     price: 5999,
+// //     category: "Party Wear",
+// //     image: "/image/party6.jpg",
+// //   },
+// //   {
+// //     id: 23,
+// //     name: "Black Elegance",
+// //     price: 6799,
+// //     category: "Party Wear",
+// //     image: "/image/party8.jpg",
+// //   },
+// //   {
+// //     id: 24,
+// //     name: "Golden Hour",
+// //     price: 7199,
+// //     category: "Party Wear",
+// //     image: "/image/party7.jpg",
+// //   },
+
+// // // FORMAL
+
+// //   {
+// //     id: 25,
+// //     name: "Pearl Grace",
+// //     price: 6499,
+// //     category: "Formal",
+// //     image: "/image/formal1.jpg",
+// //   },
+// //   {
+// //     id: 26,
+// //     name: "Ivory Charm",
+// //     price: 6299,
+// //     category: "Formal",
+// //     image: "/image/formal2.jpg",
+// //   },
+// //   {
+// //     id: 27,
+// //     name: "Emerald Bloom",
+// //     price: 6999,
+// //     category: "Formal",
+// //     image: "/image/formal3.jpg",
+// //   },
+// //   {
+// //     id: 28,
+// //     name: "Royal Blue",
+// //     price: 6799,
+// //     category: "Formal",
+// //     image: "/image/formal7.jpg",
+// //   },
+// //   {
+// //     id: 29,
+// //     name: "Champagne Silk",
+// //     price: 7299,
+// //     category: "Formal",
+// //     image: "/image/formal6.jpg",
+// //   },
+// //   {
+// //     id: 30,
+// //     name: "Classic Black",
+// //     price: 6599,
+// //     category: "Formal",
+// //     image: "/image/midnight mouse img.webp",
+// //   },
+// //   {
+// //     id: 31,
+// //     name: "Rose Gold",
+// //     price: 7199,
+// //     category: "Formal",
+// //     image: "/image/formal8.jpg",
+// //   },
+// //   {
+// //     id: 32,
+// //     name: "Elegant Beige",
+// //     price: 6499,
+// //     category: "Formal",
+// //     image: "/image/formal4.jpg",
+// //   },
+
+// //   //HOME FEATURED
+
+// //   {
+// //     id: 33,
+// //     name: "Rose Bloom Dress",
+// //     price: 4499,
+// //     category: "Featured",
+// //     image: "/image/rose blosm img.jpg",
+// //   },
+// //   {
+// //     id: 34,
+// //     name: "Lavender Dream",
+// //     price: 5499,
+// //     category: "Featured",
+// //     image: "/image/dress 2.jpg",
+// //   },
+
+// //   // ================= HOME NEW ARRIVALS =================
+
+// //   {
+// //     id: 35,
+// //     name: "Blossom Midi",
+// //     price: 4299,
+// //     category: "New Arrivals",
+// //     image: "/image/dress1.jpg",
+// //   },
+// //   {
+// //     id: 36,
+// //     name: "Satin Whisper",
+// //     price: 5899,
+// //     category: "New Arrivals",
+// //     image: "/image/dress 3.jpg",
+// //   },
+// //   {
+// //     id: 37,
+// //     name: "Ivory Charm",
+// //     price: 6299,
+// //     category: "New Arrivals",
+// //     image: "/image/dress4.jpg",
+// //   },
+// //   {
+// //     id: 38,
+// //     name: "Rosewood Dress",
+// //     price: 5499,
+// //     category: "New Arrivals",
+// //       // image: "/image/dress 5.jpg",
+// //     image:"/image/dress 5 .jpg"
+// //   },
+// // ];
+
+// // export default products;
+
 // const products = [
-//   // NEW IN
+//   // ================= NEW IN =================
 
 //   {
 //     id: 1,
 //     name: "Blossom Midi",
 //     price: 4299,
 //     category: "New In",
-//         image: "/images/midi.jpg",
-
+//     image: "/images/midi.jpg",
 //   },
+
 //   {
 //     id: 2,
 //     name: "Satin Whisper",
 //     price: 5899,
 //     category: "New In",
-//       image: "/images/satin.jpg",
-//     // image:"public/images/casual1.jpg"
+//     image: "/images/satin.jpg",
 //   },
+
 //   {
 //     id: 3,
 //     name: "Rose Charm",
@@ -24,6 +315,7 @@
 //     category: "New In",
 //     image: "/images/rose.jpg",
 //   },
+
 //   {
 //     id: 4,
 //     name: "Ivory Bloom",
@@ -31,6 +323,7 @@
 //     category: "New In",
 //     image: "/images/rose blosm img.jpg",
 //   },
+
 //   {
 //     id: 5,
 //     name: "Velvet Grace",
@@ -38,6 +331,7 @@
 //     category: "New In",
 //     image: "/images/valvet.jpg",
 //   },
+
 //   {
 //     id: 6,
 //     name: "Lavender Muse",
@@ -45,6 +339,7 @@
 //     category: "New In",
 //     image: "/images/levender.jpg",
 //   },
+
 //   {
 //     id: 7,
 //     name: "Ruby Night",
@@ -52,15 +347,16 @@
 //     category: "New In",
 //     image: "/images/red dress.jpg",
 //   },
+
 //   {
 //     id: 8,
 //     name: "Pearl Dress",
 //     price: 5799,
 //     category: "New In",
-//     image: "/images/peral.jpg",
+//     image: "/images/pearl img.webp",
 //   },
 
-//   // CASUAL
+//   // ================= CASUAL =================
 
 //   {
 //     id: 9,
@@ -69,6 +365,7 @@
 //     category: "Casual",
 //     image: "/images/casual1.jpg",
 //   },
+
 //   {
 //     id: 10,
 //     name: "Soft Bloom Midi",
@@ -76,6 +373,7 @@
 //     category: "Casual",
 //     image: "/images/causal5.jpg",
 //   },
+
 //   {
 //     id: 11,
 //     name: "Everyday Rose",
@@ -83,6 +381,7 @@
 //     category: "Casual",
 //     image: "/images/casual8.jpg",
 //   },
+
 //   {
 //     id: 12,
 //     name: "Cotton Breeze",
@@ -90,6 +389,7 @@
 //     category: "Casual",
 //     image: "/images/casual4.jpg",
 //   },
+
 //   {
 //     id: 13,
 //     name: "Pink Meadow",
@@ -97,6 +397,7 @@
 //     category: "Casual",
 //     image: "/images/casual 2.jpg",
 //   },
+
 //   {
 //     id: 14,
 //     name: "Blue Serenity",
@@ -104,6 +405,7 @@
 //     category: "Casual",
 //     image: "/images/casual6.jpg",
 //   },
+
 //   {
 //     id: 15,
 //     name: "Blush Garden",
@@ -111,6 +413,7 @@
 //     category: "Casual",
 //     image: "/images/casual7.jpg",
 //   },
+
 //   {
 //     id: 16,
 //     name: "Lavender Breeze",
@@ -119,7 +422,7 @@
 //     image: "/images/casual3.jpg",
 //   },
 
-//   // PARTY WEAR
+//   // ================= PARTY WEAR =================
 
 //   {
 //     id: 17,
@@ -128,6 +431,7 @@
 //     category: "Party Wear",
 //     image: "/images/party 1.jpg",
 //   },
+
 //   {
 //     id: 18,
 //     name: "Blush Aura",
@@ -135,123 +439,137 @@
 //     category: "Party Wear",
 //     image: "/images/party 2.jpg",
 //   },
+
 //   {
 //     id: 19,
 //     name: "Ruby Glow",
 //     price: 6499,
 //     category: "Party Wear",
-//     image: "/image/party3.jpg",
+//     image: "/images/party3.jpg",
 //   },
+
 //   {
 //     id: 20,
 //     name: "Champagne Dream",
 //     price: 6999,
 //     category: "Party Wear",
-//     image: "/image/party4.jpg",
+//     image: "/images/party4.jpg",
 //   },
+
 //   {
 //     id: 21,
 //     name: "Velvet Night",
 //     price: 6299,
 //     category: "Party Wear",
-//     image: "/image/party5.jpg",
+//     image: "/images/party5.jpg",
 //   },
+
 //   {
 //     id: 22,
 //     name: "Rose Sparkle",
 //     price: 5999,
 //     category: "Party Wear",
-//     image: "/image/party6.jpg",
+//     image: "/images/party6.jpg",
 //   },
+
 //   {
 //     id: 23,
 //     name: "Black Elegance",
 //     price: 6799,
 //     category: "Party Wear",
-//     image: "/image/party8.jpg",
+//     image: "/images/party8.jpg",
 //   },
+
 //   {
 //     id: 24,
 //     name: "Golden Hour",
 //     price: 7199,
 //     category: "Party Wear",
-//     image: "/image/party7.jpg",
+//     image: "/images/party7.jpg",
 //   },
 
-// // FORMAL
+//   // ================= FORMAL =================
 
 //   {
 //     id: 25,
 //     name: "Pearl Grace",
 //     price: 6499,
 //     category: "Formal",
-//     image: "/image/formal1.jpg",
+//     image: "/images/formal1.jpg",
 //   },
+
 //   {
 //     id: 26,
 //     name: "Ivory Charm",
 //     price: 6299,
 //     category: "Formal",
-//     image: "/image/formal2.jpg",
+//     image: "/images/formal2.jpg",
 //   },
+
 //   {
 //     id: 27,
 //     name: "Emerald Bloom",
 //     price: 6999,
 //     category: "Formal",
-//     image: "/image/formal3.jpg",
+//     image: "/images/formal3.jpg",
 //   },
+
 //   {
 //     id: 28,
 //     name: "Royal Blue",
 //     price: 6799,
 //     category: "Formal",
-//     image: "/image/formal7.jpg",
+//     image: "/images/formal7.jpg",
 //   },
+
 //   {
 //     id: 29,
 //     name: "Champagne Silk",
 //     price: 7299,
 //     category: "Formal",
-//     image: "/image/formal6.jpg",
+//     image: "/images/formal6.jpg",
 //   },
+
 //   {
 //     id: 30,
 //     name: "Classic Black",
 //     price: 6599,
 //     category: "Formal",
-//     image: "/image/midnight mouse img.webp",
+//     image: "/images/midnight mouse img.webp",
 //   },
+
 //   {
 //     id: 31,
 //     name: "Rose Gold",
 //     price: 7199,
 //     category: "Formal",
-//     image: "/image/formal8.jpg",
+//     image: "/images/formal8.jpg",
 //   },
+
 //   {
 //     id: 32,
 //     name: "Elegant Beige",
 //     price: 6499,
 //     category: "Formal",
-//     image: "/image/formal4.jpg",
+//     image: "/images/formal4.jpg",
 //   },
 
-//   //HOME FEATURED
+//   // ================= HOME FEATURED =================
 
 //   {
 //     id: 33,
 //     name: "Rose Bloom Dress",
 //     price: 4499,
 //     category: "Featured",
-//     image: "/image/rose blosm img.jpg",
+//     image: "/images/rose blosm img.jpg",
 //   },
+
 //   {
 //     id: 34,
 //     name: "Lavender Dream",
 //     price: 5499,
 //     category: "Featured",
-//     image: "/image/dress 2.jpg",
+//     image: "/images/dress 2.jpg",
 //   },
 
 //   // ================= HOME NEW ARRIVALS =================
@@ -261,34 +579,52 @@
 //     name: "Blossom Midi",
 //     price: 4299,
 //     category: "New Arrivals",
-//     image: "/image/dress1.jpg",
+//     image: "/images/dress1.jpg",
 //   },
+
 //   {
 //     id: 36,
 //     name: "Satin Whisper",
 //     price: 5899,
 //     category: "New Arrivals",
-//     image: "/image/dress 3.jpg",
+//     image: "/images/dress 3.jpg",
 //   },
+
 //   {
 //     id: 37,
 //     name: "Ivory Charm",
 //     price: 6299,
 //     category: "New Arrivals",
-//     image: "/image/dress4.jpg",
+//     image: "/images/dress4.jpg",
 //   },
+
 //   {
 //     id: 38,
 //     name: "Rosewood Dress",
 //     price: 5499,
 //     category: "New Arrivals",
-//       // image: "/image/dress 5.jpg",
-//     image:"/image/dress 5 .jpg"
+//     image: "/images/dress 5.jpg",
+//   },
+ 
+//   {
+//     id: 39,
+//     name: "Blush Elegance",
+//     price: 5799,
+//     category: "New Arrivals",
+//     image: "/images/blush aura img.jpg",
+//   },
+
+//   {
+//     id: 40,
+//     name: "Pearl Grace",
+//     price: 6299,
+//     category: "New Arrivals",
+//     image: "/images/valvet.jpg",
 //   },
 // ];
 
 // export default products;
-
+import Cart from "../Pages/Cart";
 const products = [
   // ================= NEW IN =================
 
@@ -605,13 +941,18 @@ const products = [
     category: "New Arrivals",
     image: "/images/dress 5.jpg",
   },
+
+  // ================= ID 39 =================
+
   {
     id: 39,
     name: "Blush Elegance",
     price: 5799,
     category: "New Arrivals",
-    image: "/images/shopping.webp",
+    image: "/images/blush aura img.jpg",
   },
+
+  // ================= ID 40 =================
 
   {
     id: 40,
